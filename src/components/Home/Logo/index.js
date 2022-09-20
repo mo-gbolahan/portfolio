07ae90db48efac16f60607151e36056f08644469
Gbolahan Oladeji <Gbolahan.Oladeji@egbin-power.com>
2022-09-20 15:41:00 +0100

    comment out DrawSVGPlugin

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,5 +1,5 @@
 import { useRef, useEffect } from "react";
-import DrawSVGPlugin from "gsap-trial/DrawSVGPlugin";
+// import DrawSVGPlugin from "gsap-trial/DrawSVGPlugin";
 import gsap from "gsap-trial";
 // style
 import "./logo.styles.scss";
@@ -11,28 +11,28 @@ const Logo = () => {
 	const outlineLogoRef = useRef();
 	const solidLogoRef = useRef();
 
-	useEffect(() => {
-		gsap.registerPlugin(DrawSVGPlugin);
+	// useEffect(() => {
+	// 	gsap.registerPlugin(DrawSVGPlugin);
 
-		gsap.timeline()
-			.to(bgRef.current, {
-				duration: 1,
-				opacity: 1,
-			})
-			.from(outlineLogoRef.current, {
-				drawSVG: 0,
-				duration: 10,
-			});
-		gsap.fromTo(
-			solidLogoRef.current,
-			{ opacity: 0 },
-			{
-				opacity: 1,
-				delay: 4,
-				durtion: 4,
-			}
-		);
-	}, []);
+	// 	gsap.timeline()
+	// 		.to(bgRef.current, {
+	// 			duration: 1,
+	// 			opacity: 1,
+	// 		})
+	// 		.from(outlineLogoRef.current, {
+	// 			drawSVG: 0,
+	// 			duration: 10,
+	// 		});
+	// 	gsap.fromTo(
+	// 		solidLogoRef.current,
+	// 		{ opacity: 0 },
+	// 		{
+	// 			opacity: 1,
+	// 			delay: 4,
+	// 			durtion: 4,
+	// 		}
+	// 	);
+	// }, []);
 	return (
 		<div className="logo-container" ref={bgRef}>
 			<img
