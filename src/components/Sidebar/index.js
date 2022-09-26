db060192a93d5bc43a7a4b95f6dd5b6d5da03aee
Gbolahan Oladeji <Gbolahan.Oladeji@egbin-power.com>
2022-09-26 14:24:58 +0100

    'navbar'

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,57 +4,102 @@ import "./sidebar.styles.scss";
 // img
 import LogoS from "../../assets/images/logo-s.png";
 import LogoSubtitle from "../../assets/images/logo_sub.png";
+// fontAwesomeIcon
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+	faBars,
+	faEnvelope,
+	faHome,
+	faUser,
+} from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-const Sidebar = () => (
-	<div className="nav-bar">
-		<Link className="logo" to="/">
-			<img src={LogoS} alt="logoG" />
-			<img className="sub-logo" src={LogoSubtitle} alt="Gbolahan" />
-		</Link>
-		<nav>
-			<NavLink exact="true" activeclassname="active" to="/">
-				<FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-			</NavLink>
-			<NavLink
-				exact="true"
-				activeclassname="active"
-				className="about-link"
-				to="/about"
-			>
-				<FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-			</NavLink>
-			<NavLink
-				exact="true"
-				activeclassname="active"
-				className="contact-link"
-				to="/contact"
-			>
-				<FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-			</NavLink>
-		</nav>
-		<ul>
-			<li>
-				<a
-					target="_blank"
-					rel="noreferrer"
-					href="https://www.linkedin.com/feed/"
+import { useState } from "react";
+import { useRef } from "react";
+const Sidebar = () => {
+	const navBarRef = useRef();
+	const [showNavBar, setShowNavBar] = useState(false);
+
+	return (
+		<>
+			<div className="toggle-btn btn ">
+				<div
+					onClick={(e) => {
+						e.target.classList.add("fa-beat");
+						setShowNavBar((prev) => !prev);
+					}}
+					onAnimationEnd={(e) => e.target.classList.remove("fa-beat")}
 				>
-					<FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-				</a>
-			</li>
-			<li>
-				<a
-					target="_blank"
-					rel="noreferrer"
-					href="https://github.com/mo-gbolahan"
+					<FontAwesomeIcon icon={faBars} color="#4d4d4e" beat />
+				</div>
+				<Link className="logo" to="/">
+					<img src={LogoS} alt="logoG" />
+
+					<img
+						className="sub-logo"
+						src={LogoSubtitle}
+						alt="Gbolahan"
+					/>
+				</Link>
+			</div>
+			{
+				<div
+					ref={navBarRef}
+					className={`nav-bar ${showNavBar ? "roll-out" : "roll-in"}`}
 				>
-					<FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-				</a>
-			</li>
-		</ul>
-	</div>
-);
+					<nav>
+						<NavLink exact="true" activeclassname="active" to="/">
+							<FontAwesomeIcon icon={faHome} color="#ffd700" />
+						</NavLink>
+						<NavLink
+							exact="true"
+							activeclassname="active"
+							className="about-link"
+							to="/about"
+						>
+							<FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+						</NavLink>
+						<NavLink
+							exact="true"
+							activeclassname="active"
+							className="contact-link"
+							to="/contact"
+						>
+							<FontAwesomeIcon
+								icon={faEnvelope}
+								color="#4d4d4e"
+							/>
+						</NavLink>
+					</nav>
+					<ul>
+						<li>
+							<a
+								target="_blank"
+								rel="noreferrer"
+								href="https://www.linkedin.com/feed/"
+							>
+								<FontAwesomeIcon
+									icon={faLinkedin}
+									color="#4d4d4e"
+								/>
+							</a>
+						</li>
+						<li>
+							<a
+								target="_blank"
+								rel="noreferrer"
+								href="https://github.com/mo-gbolahan"
+							>
+								<FontAwesomeIcon
+									icon={faGithub}
+									color="#4d4d4e"
+								/>
+							</a>
+						</li>
+					</ul>
+				</div>
+			}
+		</>
+	);
+};
 
 export default Sidebar;
